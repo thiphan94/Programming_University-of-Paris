252d3b3c4465e34410ca3181658da4ddc86c5662
Thi Phan <thiphan94@gmail.com>
2021-10-18 20:17:44 +0200

    update Hw2

--- Homework2/Ex1.py
+++ Homework2/Ex1.py
@@ -1,5 +1,3 @@
 # Nu Uyen Thi PHAN - M1 VMI
 def is_pal(s):
-    if len(s) <= 1:
-        return True
-    return s[0] == s[-1] and is_pal(s[1:-1])
+    return str(s) == str(s)[::-1]
--- Homework2/Ex2.py
+++ Homework2/Ex2.py
@@ -1,6 +1,11 @@
 # Nu Uyen Thi PHAN - M1 VMI
 def is_prime(a):
-    for n in range(2, int(a / 2) + 1):
-        if a % n == 0:
-            return False
-    return True
+    if type(a) != int:
+        return False
+    if a > 1:
+        for n in range(2, int(a / 2) + 1):
+            if a % n == 0:
+                return False
+        return True
+    else:
+        return False
